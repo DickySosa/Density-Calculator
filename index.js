1939ccda938635fc54cccbb8fb7418515e8b212b
DickySosa <127916303+DickySosa@users.noreply.github.com>
2023-03-28 12:46:59 -0600

    3 files modified

--- index.js
+++ index.js
@@ -2,29 +2,46 @@
 import './style.css';
 
 //Density calculation values
-
-//inputs
 let densityCalculationMass = document.querySelector(
-  '.density-calculation-mass'
-);
+  '.density-calculation-mass');
 let densityCalculationVolumen = document.querySelector(
-  '.density-calculation-volumen'
-);
-
-//button
-
+  '.density-calculation-volumen');
 let densityButton = document.querySelector('.density-button');
-
-//output
-
 let densityResult = document.querySelector('.density-result');
 
-//calculations
+function density() {
+  let result = densityCalculationMass.value / densityCalculationVolumen.value;
+  densityResult.innerHTML = result + ' g/ml';
+}
+densityButton.addEventListener('click', density);
 
 //Mass calculation values
+let massCalculationDensity = document.querySelector(
+  '.mass-calculation-density'
+);
+let massCalculationVolumen = document.querySelector(
+  '.mass-calculation-volumen'
+);
+let massButton = document.querySelector('.mass-button');
+let massResult = document.querySelector('.mass-result');
+
+function mass() {
+  let result = massCalculationDensity.value * massCalculationVolumen.value
+  massResult.innerHTML = result + ' g';
+}
+massButton.addEventListener('click', mass);
+
 
 //Volumen calculation values
 
-// Write Javascript code!
-//const appDiv = document.getElementById('app');
-//appDiv.innerHTML = `<h1>JS Starter</h1>`;
+let volumenCalculationMass = document.querySelector('.volumen-calculation-mass')
+let volumenCalculationDensity = document.querySelector('.volumen-calculation-density')
+
+let volumenButton = document.querySelector('.volumen-button')
+let volumenResult = document.querySelector('.volumen-result')
+
+function volumen(){
+  let result = volumenCalculationMass.value / volumenCalculationDensity.value
+  volumenResult.innerHTML = result + ' ml';
+}
+volumenButton.addEventListener('click', volumen)
