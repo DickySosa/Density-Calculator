128139fa1fe8f9e2bb74dfab2fc7c0a4d1cb7349
DickySosa <127916303+DickySosa@users.noreply.github.com>
2023-03-28 09:55:45 -0600

    2 files modified

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 // Import stylesheets
 import './style.css';
 
+//Density calculation values
 
+//inputs
+let densityCalculationMass = document.querySelector(
+  '.density-calculation-mass'
+);
+let densityCalculationVolumen = document.querySelector(
+  '.density-calculation-volumen'
+);
+
+//button
+
+let densityButton = document.querySelector('.density-button');
+
+//output
+
+let densityResult = document.querySelector('.density-result');
+
+//calculations
+
+//Mass calculation values
+
+//Volumen calculation values
 
 // Write Javascript code!
 //const appDiv = document.getElementById('app');
